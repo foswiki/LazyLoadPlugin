0b9101c00fa3b331dc80439f386717c47d0a4829
OlivierRaginel <OlivierRaginel@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2012-05-04 13:27:49 +0000

    Item11808: perltidy them all, so people are not accidentally impacted by the new enforced rule -- Sorry Micha, could not find a way to define specific values within the file
    git-svn-id: http://svn.foswiki.org/trunk/LazyLoadPlugin@14686 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/Foswiki/Plugins/LazyLoadPlugin/Core.pm
+++ lib/Foswiki/Plugins/LazyLoadPlugin/Core.pm
@@ -7,25 +7,25 @@ use Foswiki::Plugins::JQueryPlugin::Plugin ();
 our @ISA = qw( Foswiki::Plugins::JQueryPlugin::Plugin );
 
 sub new {
-  my $class = shift;
-  my $session = shift || $Foswiki::Plugins::SESSION;
+    my $class = shift;
+    my $session = shift || $Foswiki::Plugins::SESSION;
 
-  my $this = bless(
-    $class->SUPER::new(
-      $session,
-      name => 'LazyLoad',
-      version => '1.5.0',
-      author => 'Mika Tuupola',
-      homepage => 'http://www.appelsiini.net/projects/lazyload',
-      javascript => ['jquery.lazyload.js', 'jquery.lazyload.init.js'], 
-      documentation => 'LazyLoadPlugin',
-      puburl => '%PUBURLPATH%/%SYSTEMWEB%/LazyLoadPlugin',
-      dependencies => ["metadata"],
-    ),
-    $class
-  );
+    my $this = bless(
+        $class->SUPER::new(
+            $session,
+            name       => 'LazyLoad',
+            version    => '1.5.0',
+            author     => 'Mika Tuupola',
+            homepage   => 'http://www.appelsiini.net/projects/lazyload',
+            javascript => [ 'jquery.lazyload.js', 'jquery.lazyload.init.js' ],
+            documentation => 'LazyLoadPlugin',
+            puburl        => '%PUBURLPATH%/%SYSTEMWEB%/LazyLoadPlugin',
+            dependencies  => ["metadata"],
+        ),
+        $class
+    );
 
-  return $this;
+    return $this;
 }
 
 1;
